48e146754a9e7c4a8988ba2687b54abce6aed6a5
Maxwell Clemens <maxwell-clemens@pluralsight.com>
2018-08-06 15:42:52 -0600

    add functionality to reset quiz link

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,10 @@ class Quiz extends Component{
         this.state = {quiz_position: 1};
     }
 
+    handleResetClick(){
+        this.setState({quiz_position: 1});
+    }
+
     showNextQuestion(){
         this.setState({quiz_position: this.state.quiz_position + 1});
     }
@@ -19,7 +23,7 @@ class Quiz extends Component{
         return (
             <div>
                 {isQuizEnd ? (
-                    <QuizEnd />
+                    <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
                 ) : (
                     <QuizQuestion 
                         quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}
